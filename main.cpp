caf8ea271a40d9ad4bc7a193571d5386cbafd8df
Lion Kortlepel <development@kortlepel.com>
2020-11-24 14:15:23 +0100

    finally proper physics

--- main.cpp
+++ main.cpp
@@ -16,13 +16,15 @@ std::ostream& operator<<(std::ostream& os, const vec2& v) {
     return os << "(" << v.x << ", " << v.y << ")";
 }
 
+#define EPS 0.0001
+
 class PhysicsObject {
 public:
     vec2 pos;
     vec2 vel;
     float radius;
     float mass;
-    vec2 acc { 0 };
+    vec2 old_vel;
 
     PhysicsObject(vec2 pos, vec2 vel, float radius)
         : pos(pos)
@@ -42,9 +44,11 @@ public:
 
     void apply_forces(float dt) {
         // show forces
-        vel += acc;
+        if (glm::length(vel) < EPS || glm::isnan(glm::length(vel))) {
+            vel = vec2(0);
+        }
         pos += vel * dt;
-        acc = vec2(0);
+        old_vel = vel;
     }
 
     sf::CircleShape shape() const {
@@ -54,35 +58,48 @@ public:
         return s;
     }
 
-    static void resolve_collision(PhysicsObject& a, PhysicsObject& b) {
+    static void resolve_position(PhysicsObject& a, PhysicsObject& b) {
         vec2 direction_a_to_b = glm::normalize(b.pos - a.pos);
         vec2 direction_b_to_a = -direction_a_to_b;
         float combined_depth = a.depth_into(b);
         a.pos += direction_b_to_a * combined_depth / 2.0f;
         b.pos += direction_a_to_b * combined_depth / 2.0f;
+    }
+
+    static void resolve_collision(PhysicsObject& a, PhysicsObject& b) {
+        vec2 direction_a_to_b = glm::normalize(b.pos - a.pos);
+        vec2 direction_b_to_a = -direction_a_to_b;
 
         vec2 a_reflection_normal = direction_b_to_a;
         vec2 b_reflection_normal = direction_a_to_b;
 
-        float a_angle_vel_dir = glm::angle(glm::normalize(a.vel), direction_a_to_b);
-        float b_angle_vel_dir = glm::angle(glm::normalize(b.vel), direction_b_to_a);
+        vec2 a_reflected_vel = glm::reflect(a.old_vel, a_reflection_normal);
+        vec2 b_reflected_vel = glm::reflect(b.old_vel, b_reflection_normal);
+
+        float a_angle_vel_dir = glm::angle(glm::normalize(a.old_vel), direction_a_to_b);
+        float b_angle_vel_dir = glm::angle(glm::normalize(b.old_vel), direction_b_to_a);
         bool a_transfers_velocity = a_angle_vel_dir < glm::radians(90.0f);
         bool b_transfers_velocity = b_angle_vel_dir < glm::radians(90.0f);
         float a_transfer_percent = 1.0f - a_angle_vel_dir / glm::radians(90.0f);
         float b_transfer_percent = 1.0f - b_angle_vel_dir / glm::radians(90.0f);
 
-        vec2 new_a_vel = a.vel;
-        vec2 new_b_vel = b.vel;
-        if (a_transfers_velocity) {
-            new_b_vel += a.vel * a_transfer_percent;
-            new_a_vel -= a.vel * a_transfer_percent;
+        vec2 new_a_vel = a.old_vel;
+        vec2 new_b_vel = b.old_vel;
+        float a_len = glm::length(a.old_vel);
+        float b_len = glm::length(b.old_vel);
+        if (a_transfers_velocity && !glm::isnan(a_len) && a_len > FLT_EPSILON) {
+            new_b_vel += glm::normalize(direction_a_to_b) * a_transfer_percent * glm::length(new_a_vel);
+            new_a_vel = glm::reflect(new_a_vel * (1.0f - a_transfer_percent), a_reflection_normal);
+            //new_a_vel -= glm::normalize(b_reflected_vel) * a_transfer_percent;
         }
-        if (b_transfers_velocity) {
-            new_a_vel += b.vel * b_transfer_percent;
-            new_b_vel -= b.vel * b_transfer_percent;
+        if (b_transfers_velocity && !glm::isnan(b_len) && b_len > FLT_EPSILON) {
+            new_a_vel += glm::normalize(direction_b_to_a) * b_transfer_percent * glm::length(new_b_vel);
+            new_b_vel = glm::reflect(new_b_vel * (1.0f - b_transfer_percent), b_reflection_normal);
+            //new_b_vel -= glm::normalize(a_reflected_vel) * b_transfer_percent;
         }
         a.vel = new_a_vel;
         b.vel = new_b_vel;
+        resolve_position(a, b);
 
         /*float combined_mass = a.mass + b.mass;
         float combined_depth = a.depth_into(b);
@@ -142,8 +159,8 @@ int main() {
     //objs.emplace_back(vec2(900, 240), vec2(0, 0), 100);
     //objs.emplace_back(vec2(1100, 250), vec2(0, 0), 100);
     objs.emplace_back(vec2(0, 300), vec2(100, 0), 10);
-    for (size_t i = 0; i < 1; ++i) {
-        objs.emplace_back(vec2(300 + i * 20, 310), vec2(-100, 0), 10);
+    for (size_t i = 0; i < 3; ++i) {
+        objs.emplace_back(vec2(300 + i * 22, 310), vec2(0, 0), 10);
     }
 
     sf::Clock dt_clock;
@@ -157,9 +174,18 @@ int main() {
         sf::Text status;
         status.setFont(font);
         status.setPosition(5, 5);
+        // sum all forces
+        float forces_sum = 0;
+        for (const auto& obj : objs) {
+            float len = glm::length(obj.vel);
+            if (!glm::isnan(len)) {
+                forces_sum += len;
+            }
+        }
         std::stringstream ss;
         ss << "pause: " << (pause ? "YES" : "NO") << "\n"
-           << "gravity: " << (g_enabled ? "ON" : "OFF") << "\n";
+           << "gravity: " << (g_enabled ? "ON" : "OFF") << "\n"
+           << "sum of all forces: " << forces_sum << "\n";
         status.setString(ss.str());
         status.setFillColor(sf::Color::White);
         status.setScale(0.8, 0.8);
@@ -173,7 +199,7 @@ int main() {
         sf::Text text(std::to_string(int(glm::length(force))), font);
         vec2 mid = (pos + p2) / 2.0f;
         text.setPosition(mid.x, mid.y);
-        text.setFillColor(sf::Color::White);
+        text.setFillColor(sf::Color::Cyan);
         text.setScale(0.3, 0.3);
         window.draw(arr);
         window.draw(text);
@@ -182,9 +208,9 @@ int main() {
         float dt = dt_clock.restart().asSeconds();
         if (!pause || step) {
             for (auto& obj : objs) {
-                vec2 gravity = glm::normalize(target - obj.pos) * 10.f;
+                vec2 gravity = glm::normalize(target - obj.pos) * 1.f;
                 if (g_enabled) {
-                    obj.acc += gravity / obj.mass;
+                    obj.vel += gravity / obj.mass;
                 }
                 for (auto& other_obj : objs) {
                     if (&obj == &other_obj) {
@@ -225,6 +251,10 @@ int main() {
                 }
                 if (event.key.code == sf::Keyboard::G) {
                     g_enabled = !g_enabled;
+                }
+                if (event.key.code == sf::Keyboard::A) {
+                    auto real_pos = window.mapPixelToCoords(sf::Mouse::getPosition());
+                    objs.emplace_back(vec2(real_pos.x, real_pos.y), vec2(0), 10);
                 }
                 break;
             }
